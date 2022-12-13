c32969d0101f48b38e87c751a01ddb9bc119b3e7
Akash <shakibakashs1a@gmail.com>
2022-12-13 23:30:11 +0600

    Initial commit

---
+++ customer/src/main/java/com/fodd/freshfoodcustomer/AndroidUI/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.fodd.freshfoodcustomer.AndroidUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
